9a2b210fef968bc861725e45a76fc8478ad85bbc
anmolmann <anmolmann@gmail.com>
2017-03-29 22:40:16 +0530

    completed!

--- project.py
+++ project.py
@@ -74,25 +74,28 @@ def fbconnect():
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
 
-    url = "https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s" % (
-        app_id, app_secret, access_token
-    )
+    url = ('https://graph.facebook.com/v2.8/oauth/access_token?'
+           'grant_type=fb_exchange_token&client_id=%s&client_secret=%s'
+           '&fb_exchange_token=%s') % (app_id, app_secret, access_token)
 
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
-    # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.4/me/"
-    # strip expire tag from access token
-    token = result.split("&")[0]
+    data = json.loads(result)
 
-    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+    token = 'access_token=' + data['access_token']
+
+    # Use token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.8/me/"
+    # strip expire tag from access token
+    # token = result.split("&")[0]
+
+    url = 'https://graph.facebook.com/v2.8/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
     # print "url sent for API access:%s" % url
     print "API JSON result: %s" % result
-
 
     data = json.loads(result)
 
@@ -108,7 +111,7 @@ def fbconnect():
     login_session['access_token'] = stored_token
 
     # Get user picture
-    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.8/me/picture?%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -473,6 +476,7 @@ def deleteRestaurant(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>/')
 @app.route('/restaurant/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
+    print restaurant_id
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
 
     items = session.query(MenuItem).filter_by(
